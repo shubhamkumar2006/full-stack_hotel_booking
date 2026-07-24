1e57ca2ab9b9a848f75f5e22caf1cce2c6bc5591
shubham singe <ranjurock05@gmail.com>
2026-07-24 15:46:46 +0530

    Force Vercel serverless function rebuild for v1.0.2

--- api/index.js
+++ api/index.js
@@ -1,22 +1,6 @@
-let app;
-try {
-  app = require('../server/src/app');
-} catch (err) {
-  console.error('Failed to load Express app:', err);
-}
+// Vercel serverless entrypoint v1.0.2
+const app = require('../server/src/app');
 
 module.exports = (req, res) => {
-  if (!app) {
-    try {
-      app = require('../server/src/app');
-    } catch (err) {
-      return res.status(500).json({
-        success: false,
-        error: 'App initialization failed',
-        details: err.message,
-        stack: err.stack,
-      });
-    }
-  }
   return app(req, res);
 };
