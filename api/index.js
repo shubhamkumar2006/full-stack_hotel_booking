ca8db5c6f3ea1d3cad3b5230fd78b6743736135d
shubham singe <ranjurock05@gmail.com>
2026-07-24 15:56:20 +0530

    Configure explicit @vercel/node builder for serverless backend

--- api/index.js
+++ api/index.js
@@ -1,18 +1,3 @@
-module.exports = (req, res) => {
-  try {
-    const app = require('../server/src/app');
-    return app(req, res);
-  } catch (err) {
-    console.error('Serverless Execution Error:', err);
-    res.statusCode = 500;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(
-      JSON.stringify({
-        success: false,
-        error: 'Vercel Serverless Function Error',
-        message: err.message,
-        stack: err.stack,
-      })
-    );
-  }
-};
+const app = require('../server/src/app');
+
+module.exports = app;
